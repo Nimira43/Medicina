53ce81a7cd3717b0006a06981edd8cefe81334a3
Nimira43 <n.rabone72@aol.com>
2026-02-01 22:58:54 +0000

    Retrieving Clerk user info.

--- src/app/(private)/admin/dashboard/page.tsx
+++ src/app/(private)/admin/dashboard/page.tsx
@@ -1,10 +1,16 @@
 import { UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
 
-function DashboardPage() {
+async function DashboardPage() {
+  const user = await currentUser()
+
   return (
-    <div>
-      Dashboard Page
+    <div className='p-5 flex flex-col gap-5'>
+      <h1>Dashboard Page</h1>
       <UserButton />
+      <h1>Name: {user?.firstName} {user?.lastName}</h1>
+      <h2>ID: {user?.id}</h2>
+      <h2>Email: {user?.emailAddresses[0].emailAddress}</h2>
     </div>
   )
 }
