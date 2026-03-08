365d1e2a437600e74309633e5c56566aaabb7dd4
Nimira43 <n.rabone72@aol.com>
2026-03-08 00:54:30 +0000

    HomePage updated.

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
+import { GiCondorEmblem } from 'react-icons/gi'
 
 export default function HomePage() {
   return (
@@ -8,7 +9,6 @@ export default function HomePage() {
         <Link href='/' className='logo-text text-light text-3xl font-medium tracking-wide'>
           Medicina Healthcare
         </Link>
-
         <div className='flex gap-4'>
           <Link
             href='/sign-in'
@@ -24,22 +24,22 @@ export default function HomePage() {
           </Link>  
         </div>       
       </div>
-
-      <div className='grid grid-cols-1 md:grid-cols-2 mt-20 md:mt-32  px-10'>
-        <div className='flex flex-col gap-7 max-w-xl '>
-          <h1 className='text-4xl font-semibold text-main leading-tight'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-10 h-[80vh] items-center px-10'>
+        <div className='flex justify-center items-center md:order-last mt-10 md:mt-0'>
+          <GiCondorEmblem className='text-[250px] text-main' />
+        </div>  
+        <div className='flex flex-col items-center text-center gap-6'>
+          <h1 className='text-5xl text-main font-bold leading-tight'>
             Exceptional Care, Personalised.
           </h1>
-
-          <p className='text-base text-grey-1 leading-relaxed'>
+          <p className='text-lg text-muted-foreground leading-relaxed max-w-125'>
             Medicina Healthcare delivers private medical care built around you. Our clinicians
             combine expertise, compassion, and modern technology to provide clear answers and
             tailored treatment at every stage of your health journey. Whether you need rapid
             diagnostics, ongoing support, or specialist guidance, we ensure your experience is
             seamless, respectful, and centred on your wellbeing.
           </p>
-
-          <div className='flex gap-5 mt-2'>
+          <div className='flex gap-5 mt-2 mb-10'>
             <Button
               variant='main'
               className='text-sm transitioning'>
@@ -50,7 +50,6 @@ export default function HomePage() {
                 View Services
               </Link>
             </Button>
-
             <Button
               variant='outline'
               className='text-sm transitioning'>
@@ -62,19 +61,7 @@ export default function HomePage() {
               </Link>
             </Button>
           </div>
-        </div>
-
-        <div className='flex justify-center items-center mt-16 md:mt-0'>
-          <div className='p-2 rounded-full bg-gradient-to-br from-main to-main-dark shadow-xl ring-4 ring-main-light/40 flex justify-center items-center -mt-4'>
-            <img
-              src='/logo-4.png'
-              alt='Medicina Logo'
-              className='w-64 h-64 md:w-80 md:h-80 object-contain drop-shadow-xl'
-
-            />
-          </div>
-
-        </div>
+        </div>        
       </div>
     </div>
   )
